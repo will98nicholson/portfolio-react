b57cbdc2dbc12d0fa8b219cb4c2528db012b32b1
William Nicholson <will98nicholson@gmail.com>
2021-06-11 13:40:31 -0500

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function Header() {
+    return (
+        <div class="uk-position-relative">
+            <div class="hero">
+                <div class="herotext">
+                    <img class="bannerimage" src="Assets/Images/award.jpg" alt="seminar"></img>
+                </div>
+                <div class="banner-overlay-div"></div>
+                <div class="herotext">
+                    <h2>Hello, I'm William</h2>
+                </div>
+            </div>
+        </div>
+
+    )
+}
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,4 +27,3 @@ export function Col({ size, children }) {
     );
 }
 
-c
